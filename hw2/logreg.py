59259a17266420a4085684d624cf7ecbe31771ea
Adam B Yedidia <adamy@mit.edu>
2014-11-02 22:06:21 -0500

    added logreg.py

---
+++ hw2/logreg.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+
+class KLR:
+	def __init__(self, X):
+		self.xxt = X.dot(X.transpose())
+	
+	def nll(alpha):
+		
+
+def nll(alpha):
+	X = np.array([[1, 0], [1, 2], [1, 3]])
+	Y = np.array([[-1], [1], [1]])
+	
+	dPlusOne = np.size(X, 1)
+	n = np.size(X, 0)	
+
+	sumOfLogs = 0
+	for i in range(n):
+		sumOfLogs += 
+
+	return 
+
+
