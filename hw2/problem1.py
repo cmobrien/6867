3b7da79a44132a71352c23e5d8a6a6def8df2a1f
Casey M O'Brien <cmobrien@mit.edu>
2014-11-03 18:27:46 -0500

    Working on problem 1

--- hw2/problem1.py
+++ hw2/problem1.py
@@ -1,27 +1,62 @@
-
+from cvxopt import matrix, solvers
 
 def dot(x1, x2):
   if len(x1) != len(x2):
     assert False
   return sum([x1[i] * x2[i] for i in range(len(x1))])
 
+def scalar_mult(x, a):
+    return [elem * a for elem in x]
+
+def component_wise_add(x1, x2):
+    if len(x1) != len(x2):
+        assert False
+    return [x1[i] + x2[i] for i in range(len(x1))]
+
 def build_matrices(X, Y, C):
   n = len(X)
-  p = [-1]*n
+  q = [-1]*n
   G = [[0]*(2 * i) + [1, -1] + [0] * (2*(n - i - 1)) for i in range(n)]
   h = [C, 0]*n
-  A = Y
+  A = [[y] for y in Y]
   b = [0]
-  Q = [[0] * n for i in range(n)]
+  P = [[0] * n for i in range(n)]
   for i in range(n):
     for j in range(n):
-      if i == j:
-        Q[i][j] = 0.5 * Y[i] ** 2 * (dot(X[i], X[i]))
-      else:
-        Q[i][j] = 0.25 * Y[i] * Y[j] * (dot(X[i], X[j]))
-  print Q
-  return Q, p, G, h, A, b
+        P[i][j] = Y[i] * Y[j] * (dot(X[i], X[j]))
+  return P, q, G, h, A, b
 
-build_matrices([[1], [2], [3], [4], [5]], [1, 2, 3, 4, 5], 1)
 
+def solve_qp(X, Y, C):
+    P, q, G, h, A, b = build_matrices(X, Y, C)
+    sol = solvers.qp(matrix(P, tc = 'd'), matrix(q, tc = 'd'), matrix(G, tc = 'd'), matrix(h, tc = 'd'), matrix(A, tc = 'd'), matrix(b, tc = 'd')) 
+    return sol['x']
+
+
+X = [[1, 2], [2, 2], [0, 0], [-2, 3]]
+Y = [1, 1, -1, -1]
+alpha = solve_qp(X, Y, 1)
+
+def get_weights(X, Y, alpha):
+    w = [0] * len(X[0])
+    for i in range(len(X)):
+        term = scalar_mult(X[i], alpha[i] * Y[i])
+        w = component_wise_add(w, term)
+    return [get_w0(X, Y, alpha)] + w
+
+THRESHOLD = 0.0000001
+
+def get_w0(X, Y, alpha):
+    s = 0
+    count = 0
+    for j in range(len(X)):
+        if alpha[j] > THRESHOLD:
+            inner = 0
+            for i in range(len(X)):
+                inner += alpha[i] * Y[i] * dot(X[i], X[j])
+            s += Y[j] - inner
+            count += 1
+    return s/count
+        
+print get_weights(X, Y, alpha)
 
--- hw2/svm_test.py
+++ hw2/svm_test.py
@@ -1,20 +1,40 @@
 from numpy import *
 from plotBoundary import *
 # import your SVM training code
+from problem1 import *
 
 # parameters
-name = 'ls'
+name = 'stdev4'
 print '======Training======'
 # load data from csv files
-train = loadtxt('data/data_'+name+'_train.csv')
+train = loadtxt('newData/data_'+name+'_train.csv')
 # use deep copy here to make cvxopt happy
 X = train[:, 0:2].copy()
 Y = train[:, 2:3].copy()
 
+X_list = X.tolist()
+Y_list = [y[0] for y in Y.tolist()]
 # Carry out training, primal and/or dual
-### TODO ###
+alpha = solve_qp(X_list, Y_list, 1)
+w = get_weights(X_list, Y_list, alpha)
+print "W: ", w
 # Define the predictSVM(x) function, which uses trained parameters
-### TODO ###
+def predictSVM(x):
+    val = w[0] + sum([w[i + 1] * x[i] for i in range(len(x))]) 
+    if val > 0:
+        return 1.0
+    else:
+        return -1.0
+
+def error_rate(X, Y):
+    errors = 0
+    for i in range(len(X)):
+        guess = predictSVM(X[i])
+        if guess != Y[i]:
+            errors += 1
+    return errors
+
+print "TEST ERROR: ", error_rate(X, Y)
 
 # plot training results
 plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
@@ -22,9 +42,13 @@ plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
 
 print '======Validation======'
 # load data from csv files
-validate = loadtxt('data/data_'+name+'_validate.csv')
+validate = loadtxt('newData/data_'+name+'_validate.csv')
 X = validate[:, 0:2]
 Y = validate[:, 2:3]
+
+print "VALIDATION ERROR: ", error_rate(X, Y)
+
 # plot validation results
 plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+
 
