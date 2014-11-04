b87cdb542556e7630af4b7e2e1b46b669f5cab9f
Michelle Cunningham <mcunning@mit.edu>
2014-11-04 00:43:41 -0500

    Problem 1

--- hw2/problem1.py
+++ hw2/problem1.py
@@ -1,5 +1,7 @@
 from cvxopt import matrix, solvers
+import math
 
+################################################################################
 def dot(x1, x2):
   if len(x1) != len(x2):
     assert False
@@ -13,8 +15,21 @@ def component_wise_add(x1, x2):
         assert False
     return [x1[i] + x2[i] for i in range(len(x1))]
 
-def build_matrices(X, Y, C):
-  n = len(X)
+def component_wise_subtract(x1, x2):
+  if len(x1) != len(x2):
+    assert False
+  return [x1[i] - x2[i] for i in range(len(x1))]
+################################################################################
+
+def dot_kernel(X):
+  return lambda xi, xj: dot(xi, xj)
+
+def gaussian_kernel(X, beta):
+  return lambda xi, xj: math.e ** (-1 * beta * dot(component_wise_subtract(xi, xj), component_wise_subtract(xi, xj)))
+    
+
+def build_matrices(kernel, X, Y, C):
+  n = len(Y)
   q = [-1]*n
   G = [[0]*(2 * i) + [1, -1] + [0] * (2*(n - i - 1)) for i in range(n)]
   h = [C, 0]*n
@@ -23,19 +38,25 @@ def build_matrices(X, Y, C):
   P = [[0] * n for i in range(n)]
   for i in range(n):
     for j in range(n):
-        P[i][j] = Y[i] * Y[j] * (dot(X[i], X[j]))
+        P[i][j] = Y[i] * Y[j] * (kernel(X[i], X[j]))
   return P, q, G, h, A, b
 
 
-def solve_qp(X, Y, C):
-    P, q, G, h, A, b = build_matrices(X, Y, C)
+def solve_qp(kernel, X, Y, C):
+    P, q, G, h, A, b = build_matrices(kernel, X, Y, C)
+    print matrix(P).size
+    print matrix(q).size
+    print matrix(G).size
+    print matrix(h).size
+    print matrix(A).size
+    print matrix(b).size
     sol = solvers.qp(matrix(P, tc = 'd'), matrix(q, tc = 'd'), matrix(G, tc = 'd'), matrix(h, tc = 'd'), matrix(A, tc = 'd'), matrix(b, tc = 'd')) 
     return sol['x']
 
 
-X = [[1, 2], [2, 2], [0, 0], [-2, 3]]
-Y = [1, 1, -1, -1]
-alpha = solve_qp(X, Y, 1)
+#X = [[1, 2], [2, 2], [0, 0], [-2, 3]]
+#Y = [1, 1, -1, -1]
+#alpha = solve_qp(X, Y, 1)
 
 def get_weights(X, Y, alpha):
     w = [0] * len(X[0])
@@ -58,5 +79,7 @@ def get_w0(X, Y, alpha):
             count += 1
     return s/count
         
-print get_weights(X, Y, alpha)
+#print get_weights(X, Y, alpha)
+
+
 
--- hw2/svm_test.py
+++ hw2/svm_test.py
@@ -4,7 +4,7 @@ from plotBoundary import *
 from problem1 import *
 
 # parameters
-name = 'stdev4'
+name = 'nonSep2'
 print '======Training======'
 # load data from csv files
 train = loadtxt('newData/data_'+name+'_train.csv')
@@ -15,7 +15,9 @@ Y = train[:, 2:3].copy()
 X_list = X.tolist()
 Y_list = [y[0] for y in Y.tolist()]
 # Carry out training, primal and/or dual
-alpha = solve_qp(X_list, Y_list, 1)
+#f = gaussian_kernel(X_list, 1)
+f = dot_kernel(X_list)
+alpha = solve_qp(f, X_list, Y_list, 1)
 w = get_weights(X_list, Y_list, alpha)
 print "W: ", w
 # Define the predictSVM(x) function, which uses trained parameters
@@ -26,6 +28,19 @@ def predictSVM(x):
     else:
         return -1.0
 
+def predictSVM3(x):
+  print x
+  val = 0
+  for i in range(len(X)):
+    val += Y[i] * alpha[i] *f(X[i], x)
+  if val > 0:
+    return 1.0
+  else:
+    return -1.0
+
+def geometric_margin(w):
+  return 1 / math.sqrt(dot(w, w))
+
 def error_rate(X, Y):
     errors = 0
     for i in range(len(X)):
@@ -35,6 +50,7 @@ def error_rate(X, Y):
     return errors
 
 print "TEST ERROR: ", error_rate(X, Y)
+print "MARGIN: ", geometric_margin(w)
 
 # plot training results
 plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
@@ -43,12 +59,20 @@ plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Train')
 print '======Validation======'
 # load data from csv files
 validate = loadtxt('newData/data_'+name+'_validate.csv')
-X = validate[:, 0:2]
-Y = validate[:, 2:3]
+X_val = validate[:, 0:2]
+Y_val = validate[:, 2:3]
 
 print "VALIDATION ERROR: ", error_rate(X, Y)
 
 # plot validation results
-plotDecisionBoundary(X, Y, predictSVM, [-1, 0, 1], title = 'SVM Validate')
+plotDecisionBoundary(X_val, Y_val, predictSVM, [-1, 0, 1], title = 'SVM Validate')
 
-
+#d = {}
+#for b in range(1, 100, 1):
+#  f = gaussian_kernel(X_list, b)
+#  alpha = solve_qp(f, Y_list, 1)
+#  w = get_weights(X_list, Y_list, alpha)
+#  d[b] = error_rate(X_val, Y_val)
+#
+#for b in range(1, 100, 1):
+#  print b, d[b]
