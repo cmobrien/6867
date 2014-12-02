7524c965f4beed8fede45f6fc14f0101d425102d
Casey O'Brien <cmobrien@mit.edu>
2014-12-02 16:53:21 -0500

    work

--- project/r.py
+++ project/r.py
@@ -1,5 +1,6 @@
 from make_features import *
 from scipy.stats import norm
+import math
 
 def dot(x1, x2):
   assert len(x1) == len(x2)
@@ -44,19 +45,6 @@ for i in range(1, len(f)):
 #w = [0, 0.5505, 0.6429, 0.864, 0.4661, 1.0566, 0.459, 0.8737, 0.6001, 0.9068]
 #w = [0, 0.361396, 0.462982, 0.442022, 0.301857, 0.616841, 0.284195, 0.487308, 0.346440, 0.503130, 0.467774, -3.288724, -0.228585, 0.049822, 0.122946, 0.487668, -0.005383, 0.152088]
     #w = [0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-X, Y = get_data(4, "test.csv") 
-Y_letter = [y[1] for y in Y]
-guess = []
-
-for i in range(len(X)):
-  val = predict(X[i], Y_letter[i], w)
-  if val > 0.5111:
-    guess.append("A")
-  elif val > -1.8335:
-    guess.append("B")
-  else:
-    guess.append("C")
-
 
 def get_dist(pred, mA, mB):
   A = 1 - norm.cdf(mA - pred)
@@ -64,8 +52,42 @@ def get_dist(pred, mA, mB):
   C = norm.cdf(mB - pred)
   return (A, B, C)
 
+def get_penalty(G, dist):
+  if G == "A":
+    return math.log(dist[0])
+  if G == "B":
+    return math.log(dist[1])
+  if G == "C":
+    return math.log(dist[2])
+
+X, Y = get_data(4, "test.csv") 
+Y_letter = [y[1] for y in Y]
+guess = []
+mA = 0.5111
+mB = -1.8335
+
+
+penalty = 0
+for i in range(len(X)):
+  val = predict(X[i], Y_letter[i], w)
+  dist = get_dist(val, mA, mB)
+  if val > 0.5111:
+    guess.append("A")
+  elif val > -1.8335:
+    guess.append("B")
+  else:
+    guess.append("C")
+  p = get_penalty(Y_letter[i], dist)
+  penalty += p
+  print dist
+  print Y_letter[i]
+  print p
+
 errors, mis = calculate_error(guess, Y_letter)
 print errors
+print penalty
+
+
 
 THRESHOLD = 0.8
 for m in mis:
